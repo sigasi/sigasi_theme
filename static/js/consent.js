ef8e01e8cbe440f9cf0ffd0c4ae891669f527424
Titouan Vervack <titouan.vervack@sigasi.com>
2023-10-20 14:27:58 +0200

    Adhere to DNT and Sec-GPC

--- static/js/consent.js
+++ static/js/consent.js
@@ -1,4 +1,15 @@
 (function () {
+    // Don't unhide cookie banner if DNT or Sec-GPC is on
+    if (
+      navigator.doNotTrack == "yes" ||
+      navigator.doNotTrack == "1" ||
+      navigator.msDoNotTrack == "1" ||
+      navigator.globalPrivacyControl == "1"
+    ) {
+      return;
+    }
+
+    // Unhide cookie banner cookies are allowed
     if (document.cookie.indexOf("viewed_cookie_policy=yes") === -1) {
         document.getElementById("cookie_consent").classList.remove("uk-hidden");
         document.getElementById("close_cookie_consent").onclick = function () {
